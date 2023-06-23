5f25724085dadbba9eb56776f0fc16aaefd25231
sergio Garcia <ch3cho500@gmail.com>
2023-06-22 23:15:14 -0500

    initial scaffolding

---
+++ src/test/java/com/grahql/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grahql.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
